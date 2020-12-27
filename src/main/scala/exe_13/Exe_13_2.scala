5de0c7a2ad0561f236cda0f1f9502b15fe250e77
Alan Mazankiewicz <alan.mazankiewicz@live.de>
2020-12-27 18:57:34 +0100

    solved exe13_pt2

--- src/main/scala/exe_13/Exe_13_2.scala
+++ src/main/scala/exe_13/Exe_13_2.scala
@@ -12,43 +12,28 @@ object Exe_13_2 {
     val data_path = "src/main/scala/exe_13/data.txt"
     val data = Source.fromFile(data_path).getLines.toVector
 
-    def test(value: Long):Unit = { // TODO Remove
-      if(value < 0) println(value)
-    }
-
     def parse_input(input: Vector[String]): List[(Long, Long)] = {
       val splitted = input(1).split(",")
       val zipped = splitted.zipWithIndex
       (zipped filter { x => x._1 != "x" } map { x => (x._1.toLong, x._2.toLong) }).toList
     }
 
-    def next_divisor_of_k_greater_x(x: Long)(k: Long): Long = {
-      val remainder = (x + k) % k
-      x + k - remainder
-    }
-
     def run(input: Vector[String]): Long = {
-      val parsed_input = parse_input(input)
-      val base_val = parsed_input.head._1
-      val busses = parsed_input.tail
+      val busses = parse_input(input)
 
-      def next_timestamp(last_timestamp: Long): Long = last_timestamp + base_val
+      def check(current_timestamp: Long, busses: List[(Long, Long)], step_size: Long): (List[(Long, Long)], Long) = {
+        if ((busses == Nil) || ((current_timestamp + busses.head._2) % busses.head._1 != 0))
+          (busses, step_size)
+        else  check(current_timestamp, busses.tail, step_size * busses.head._1) // The last arg is the key to solving that exe efficiently
 
-      def check(current_timestamp: Long, busses: List[(Long, Long)], current_result: Boolean): Boolean = {
-        if (!current_result || busses == Nil) current_result
-        else {
-          val (bus_id, offset) = busses.head
-          val normalized_timestamp = next_divisor_of_k_greater_x(current_timestamp)(bus_id) - offset
-          check(current_timestamp, busses.tail, current_timestamp == normalized_timestamp)
-        }
       }
 
-      def loop(current_timestamp: Long): Long = {
-        test(current_timestamp)
-        if(check(current_timestamp, busses, true)) current_timestamp
-        else loop(next_timestamp(current_timestamp))
+      def loop(current_timestamp: Long, busses: List[(Long, Long)], step_size: Long): Long = {
+        val (new_busses, new_step_size) = check(current_timestamp, busses, step_size)
+        if (new_busses == Nil) current_timestamp
+        else loop(current_timestamp + new_step_size, new_busses, new_step_size)
       }
-      loop(0)
+      loop(1, busses, 1)
     }
 
 
