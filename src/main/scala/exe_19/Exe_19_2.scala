1ee93544c7842b1f178f9c719cc898c8d23e327f
Alan Mazankiewicz <alan.mazankiewicz@live.de>
2020-12-31 13:12:59 +0100

    checkpoint

--- src/main/scala/exe_19/Exe_19_2.scala
+++ src/main/scala/exe_19/Exe_19_2.scala
@@ -7,8 +7,8 @@ object Exe_19_2 {
   def main(args: Array[String]): Unit = {
 
     val path = "src/main/scala/exe_19/"
-    val rules = Source.fromFile(path + "rules.txt").getLines.toList
-    val data = Source.fromFile(path + "data.txt").getLines.toList
+    val rules = Source.fromFile(path + "test_rules.txt").getLines.toList
+    val data = Source.fromFile(path + "test_data.txt").getLines.toList
 
     def parse_rule(parsed_rules: Map[String, List[List[String]]], rule: String): Map[String, List[List[String]]] = {
       val splitted = rule.split(": ")
@@ -30,9 +30,7 @@ object Exe_19_2 {
       data.foldLeft(empty_set)({ (set, x) => set + x })
     }
 
-    def count_valid_rules(rules_map: Map[String, List[List[String]]], data: Set[String], limit_inifinity: Int): Int = {
-
-      var limit = limit_inifinity
+    def count_valid_rules(rules_map: Map[String, List[List[String]]], data: Set[String]): Int = {
 
       def combine_lists(fst: List[String], sec: List[String]): List[String] = {
         for {
@@ -54,8 +52,40 @@ object Exe_19_2 {
         rules flatMap process_rule
       }
 
-      val all_rules = process_rules(rules_map("0"))
-      all_rules count { x => data.contains(x) }
+      def find_startsWith(data_point: String, prefixes: List[String]): Boolean = {
+        prefixes find { x => data_point startsWith(x) } match {
+          case Some(i) => true
+          case None => false
+        }
+      }
+
+      def check(data_point_check: String => Boolean, prefixes: List[String]): Boolean = {
+        prefixes find { x => data_point_check(x) } match {
+          case Some(i) => true
+          case None => false
+        }
+      }
+
+      val test = Set("bbabbbbaabaabba", "babbbbaabbbbbabbbbbbaabaaabaaa", "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+      "bbbbbbbaaaabbbbaaabbabaaa", "bbbababbbbaaaaaaaabbababaaababaabab", "ababaaaaaabaaab", "ababaaaaabbbaba", "baabbaaaabbaaaababbaababb",
+      "abbbbabbbbaaaababbbbbbaaaababb", "aaaaabbaabaaaaababaa", "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa", "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba")
+
+
+      val eight_rules = process_rules(rules_map("8"))
+      val prefixes = eight_rules ::: combine_lists(eight_rules, eight_rules)// TODO
+
+      val eleven_rules = process_rules(rules_map("11"))
+      val th_rules = process_rules(rules_map("31"))
+      // val suffixes = eleven_rules ::: combine_lists(eleven_rules, th_rules)
+      val suffixes = th_rules
+
+      val count = data filter { x => check(x.startsWith, prefixes) && check(x.endsWith, suffixes) }
+
+
+
+
+
+      0 // count
     }
 
     println(count_valid_rules(parse_all_rules(rules), parse_data(data)))
