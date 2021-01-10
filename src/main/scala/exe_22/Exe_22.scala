de666923efc8f4cf78ec5f36a90ad73fda64c112
Alan Mazankiewicz <alan.mazankiewicz@live.de>
2021-01-10 20:10:09 +0100

    finished exe22pt2

--- src/main/scala/exe_22/Exe_22.scala
+++ src/main/scala/exe_22/Exe_22.scala
@@ -11,13 +11,30 @@ object Exe_22 {
     val raw_p1 = Source.fromFile(path + "player_1.txt").getLines.toList
     val raw_p2 = Source.fromFile(path + "player_2.txt").getLines.toList
 
-    class Player(raw_deck: List[String]) {
-      private val deck = mutable.Queue(raw_deck map { x => x.toInt }: _*)
+    class Player(val id: String) {
+      var deck: mutable.Queue[Int] = mutable.Queue()
+
+      def create_from_scratch(raw_deck: List[String]): Unit = {
+        deck = mutable.Queue(raw_deck map { x => x.toInt }: _*)
+      }
+
+      def create_from_queue(new_deck: mutable.Queue[Int]): Unit = {
+        deck = new_deck
+      }
+
+      def copy(size: Int): Player = {
+        val new_player = new Player(this.id)
+        new_player.create_from_queue(deck.take(size))
+        new_player
+      }
+
+      def size(): Int = deck.size
 
       def draw(): Int = deck.dequeue()
 
       def add_deck(win_card: Int, loose_card: Int): Unit = {
-        deck += win_card; deck += loose_card
+        deck += win_card;
+        deck += loose_card
       }
 
       def notEmpty(): Boolean = deck.nonEmpty
@@ -37,17 +54,53 @@ object Exe_22 {
     }
 
     def play_match(fst: Player, sec: Player): Player = {
-      while(fst.notEmpty() && sec.notEmpty()){
+      while (fst.notEmpty() && sec.notEmpty()) {
         play_round(fst, sec)
       }
       if (fst.notEmpty()) fst
       else sec
     }
 
-    val fst = new Player(raw_p1)
-    val sec = new Player(raw_p2)
+    val fst = new Player("1")
+    val sec = new Player("2")
+    fst.create_from_scratch(raw_p1)
+    sec.create_from_scratch(raw_p2)
     val winning_score = play_match(fst, sec).get_score()
     println(winning_score)
+
+    // part_2
+
+    def play_match_2(fst: Player, sec: Player): Player = {
+      val mem: mutable.ArrayBuffer[mutable.Queue[Int]] = mutable.ArrayBuffer()
+
+      def play_round(fst: Player, sec: Player): Unit = {
+        mem += fst.deck.clone()
+        val fst_card = fst.draw()
+        val sec_card = sec.draw()
+
+        if (fst.size() >= fst_card && sec.size() >= sec_card) {
+          val winner = play_match_2(fst.copy(fst_card), sec.copy(sec_card))
+          if (winner.id == "1") fst.add_deck(fst_card, sec_card)
+          else sec.add_deck(sec_card, fst_card)
+        }
+        else {
+          if (fst_card > sec_card) fst.add_deck(fst_card, sec_card)
+          else sec.add_deck(sec_card, fst_card)
+        }
+      }
+
+      while (fst.notEmpty() && sec.notEmpty()) {
+        if(mem.contains(fst.deck)) return fst
+        else play_round(fst, sec)
+      }
+      if (fst.notEmpty()) fst
+      else sec
+    }
+
+    fst.create_from_scratch(raw_p1)
+    sec.create_from_scratch(raw_p2)
+    val score_2 = play_match_2(fst, sec).get_score()
+    println(score_2)
 
     // END
   }
